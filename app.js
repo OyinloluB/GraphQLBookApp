2c88e5102cf83a0b7a1dc76fdce49f0419b1fcb8
Oyinlolu <oyinloluwab@gmail.com>
2021-01-22 13:50:48 +0100

    database connected

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connect to mDB
+mongoose.connect('mongodb+srv://Oyinloluwa:Oyinloluwa@routing.3vlsj.mongodb.net/Oyinloluwa?retryWrites=true&w=majority')
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+});
 
 // bind express with graphql
 app.use(
